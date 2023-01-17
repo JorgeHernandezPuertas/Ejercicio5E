a5e99894f48c9826819dd99da2947b15351bc03e
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-01-17 12:21:33 +0100

    Cambia la clase coordenada de una record a una normal porque necesito modificar los valores una vez creada para ahorrar memoria.

--- src/main/java/ejercicio5e/Coordenada.java
+++ src/main/java/ejercicio5e/Coordenada.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ejercicio5e;
 
@@ -8,6 +8,84 @@ package ejercicio5e;
  *
  * @author jorge
  */
-public record Coordenada(int coordenadaX, int coordenadaY, int elemento) {
+public class Coordenada {
+    
+    // Atributos
+    private int coordenadaX;
+    private int coordenadaY;
+    private int elemento;
+    
+    // Constructor
+    public Coordenada() {
 
+    }
+    
+    // Setters
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    public void setElemento(int elemento) {
+        this.elemento = elemento;
+    }
+    
+    // Getters
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    public int getElemento() {
+        return elemento;
+    }
+    
+    // Método toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Coordenada{");
+        sb.append("coordenadaX=").append(coordenadaX);
+        sb.append(", coordenadaY=").append(coordenadaY);
+        sb.append(", elemento=").append(elemento);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    // Métodos equals y hashCode
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + this.coordenadaX;
+        hash = 67 * hash + this.coordenadaY;
+        hash = 67 * hash + this.elemento;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordenada other = (Coordenada) obj;
+        if (this.coordenadaX != other.coordenadaX) {
+            return false;
+        }
+        if (this.coordenadaY != other.coordenadaY) {
+            return false;
+        }
+        return this.elemento == other.elemento;
+    }
 }
